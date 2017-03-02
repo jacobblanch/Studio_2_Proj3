31397e7ff6ff7b56e3a6d9f31bb1b1905f160435
SAE\amcclenaghan <1004436@student.sae.edu.au>
2017-03-03 09:57:44 +1000

    Put Jacobs track in scene1. Added barrier with trees on them to straight roads. Added a few more fighter jets. Also made a plane below the track where if you fall off it resets you to the start of the track

---
+++ SynthRacerupload2/TestProj3/Assets/scripts/fallcheck.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fallcheck : MonoBehaviour {
+
+    public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void MakePlayerReset( )
+    {
+        Player.transform.position = new Vector3(0, 0, 0);
+        Player.transform.rotation = Quaternion.Euler(0, 0, 0);
+        Player.GetComponent<Rigidbody>().velocity = new Vector3( 0,0,0);
+        //StopAllCoroutines();
+        //StartCoroutine(playerLerp(Player.transform.position, Player.transform.rotation));
+    
+    } 
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.collider.transform.tag == "Player")
+        {
+            MakePlayerReset();
+        }
+    }
+
+
+    IEnumerator playerLerp(Vector3 playerpos, Quaternion playerrot)
+    {
+        Vector3 playerend = new Vector3(0, 1, 0);
+        Quaternion rotend = Quaternion.Euler(0, 0, 0);
+        while (playerpos != playerend)
+        {
+            Player.transform.position = Vector3.Lerp(playerpos, playerend, 1 * Time.deltaTime);
+
+            while (playerrot != rotend)
+            {
+                Player.transform.rotation = Quaternion.Lerp(playerrot, rotend, 1 * Time.deltaTime);
+            }
+        }
+
+     
+        yield return null;
+    }
+}
--- SynthRacerupload2/TestProj3/Assets/scripts/turbo.cs
+++ SynthRacerupload2/TestProj3/Assets/scripts/turbo.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class turbo : MonoBehaviour {
 
     public GameObject turbopoint;
+    public GameObject warpeffects;
+    public GameObject leftbraketrail;
+    public GameObject rightbraketrail;
 
     void Start ()
     {
@@ -13,24 +16,32 @@ public class turbo : MonoBehaviour {
 	
 	void Update ()
     {
-		if (Input.GetKeyDown(KeyCode.Escape))
+		if (Input.GetKeyDown(KeyCode.Q))
         {
             TURBO();
         }
-        if (Input.GetKeyUp(KeyCode.Escape))
+        if (Input.GetKeyUp(KeyCode.Q))
         {
             NomoreTURBO();
+        }
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            Application.Quit();
         }
     }
 
 
     void TURBO()
     {
-
+        rightbraketrail.SetActive(true);
+        leftbraketrail.SetActive(true);
+        warpeffects.SetActive(true);
     }
 
     void NomoreTURBO()
     {
-
+        rightbraketrail.SetActive(false);
+        leftbraketrail.SetActive(false);
+        warpeffects.SetActive(false);
     }
 }
