49863a17a5b51341c846230f38ddfdee1ba525c8
Jacob Blanch <jblanch3@sae.au>
2017-03-06 18:02:43 +1000

    Pushing end of mondays work

---
+++ SynthRacerupload2/TestProj3/Assets/scripts/CarController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour {
+    public GameObject pivot;
+    public float speed;
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position = pivot.transform.position + new Vector3(0,0.5f,1.5f);
+        transform.rotation = pivot.transform.rotation;
+        PlayerControls();
+	}
+
+    public void PlayerControls()
+    {
+        while (Input.GetKeyDown("w"))
+        {
+            Debug.Log("GOING");
+            rb.AddForce(transform.forward * speed * Time.deltaTime);
+        }
+    }
+}
