d208863153c3c5410c12cac0a35b4d06d44f85ea
SAE\amcclenaghan <1004436@student.sae.edu.au>
2017-03-07 12:51:20 +1000

    made ring of fighters fly around whole map. yellwo ships in large honeycomb at top now rotate.

---
+++ SynthRacerupload2/TestProj3/Assets/honeycombrotate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class honeycombrotate : MonoBehaviour
+{
+
+    public GameObject[] objs;
+
+
+    private float rotationspeed;
+
+    public bool rotate = false;
+    private float rottaroundtrackstart = 8.9f;
+
+
+
+    void Start()
+    {
+        rotationspeed = Random.Range(16, 22);
+    }
+
+    void Update()
+    {
+        rottaroundtrackstart -= Time.deltaTime;
+        if (rottaroundtrackstart <= 0)
+        {
+            rotate = true;
+        }
+
+        if (rotate)
+        {
+            foreach (GameObject obj in objs)
+            {
+                obj.transform.Rotate(Vector3.up * rotationspeed * Time.deltaTime * 3.5f);
+            }
+
+
+        }
+    }
+}
