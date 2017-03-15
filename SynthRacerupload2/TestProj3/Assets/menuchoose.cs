40380866611df9d0f4c45bbb495af9dfea38fd33
SAE\amcclenaghan <1004436@student.sae.edu.au>
2017-03-15 21:03:26 +1000

    menu choosing starting to take effect. not completed.

---
+++ SynthRacerupload2/TestProj3/Assets/menuchoose.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuchoose : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+
+    public void ChooseDrive()
+    {
+        PlayerPrefs.SetInt("MenuCHoice", 1);
+    }
+
+
+    public void ChooseCruise()
+    {
+        PlayerPrefs.SetInt("MenuCHoice", 2);
+    }
+}
---
+++ SynthRacerupload2/TestProj3/Assets/scenechooser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scenechooser : MonoBehaviour {
+
+    private int selector;
+
+
+	void Start () {
+        selector = PlayerPrefs.GetInt("MenuCHoice");
+
+        if (selector == 1)
+        {
+            Drive();
+        }
+
+        if (selector == 2)
+        {
+            Cruise();
+        }
+	}
+	
+	void Drive()
+    {
+
+    }
+
+    void Cruise()
+    {
+
+    }
+}
